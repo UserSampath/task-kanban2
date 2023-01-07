e83d0aa0b439b811cd02dca880e72b802b432bd1
UserSampath <chandrasiriwns.20@itfac.mrt.ac.lk>
2023-01-07 23:42:27 +0530

    after addnig css

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import './App.css';
 import DragNDrop from './components/DragNDrop';
 
 const data = [
-  { title: 'group1', items: ['1', '2', '3'] },
-  { title: 'group2', items: ['4', '5', '6'] }]
+  { title: 'To Do', items: ['Task 1', 'Task 2', 'Task 3'] },
+  { title: 'Started', items: ['Task 4', 'Task 5'] },
+  { title: 'QA', items: ['Task 6', 'Task 7', 'Task 8', 'Task 9'] },
+  { title: 'Done', items: ['Task 6'] }
+]
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <DragNDrop data={ data} />
+        <DragNDrop data={data} />
       </header>
     </div>
   );
