54a67dee4e4a2f6accc0fa8ade8f54452aac0321
UserSampath <chandrasiriwns.20@itfac.mrt.ac.lk>
2023-01-07 17:45:28 +0530

    display entering drag

--- src/components/DragNDrop.js
+++ src/components/DragNDrop.js
@@ -1,10 +1,39 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 
 export const DragNDrop = ({ data }) => {
     const [list, setList] = useState(data);
+    const [dragging, setDragging] = useState(false);
+    const dragItem = useRef();
+    const dragNode = useRef();
 
     const handleDragStart = (e, params) => {
         console.log('drag start', params);
+        dragItem.current = params;
+        dragNode.current = e.target;
+        dragNode.current.addEventListener('dragend', handleDragEnd);
+        setTimeout(() => {
+            setDragging(true);
+        }, 0)
+    }
+
+    const handleDragEnd = () => {
+        console.log('ending drag..');
+        setDragging(false);
+        dragNode.current.removeEventListener('dragend', handleDragEnd);
+        dragNode.current = null;
+        dragItem.current = null;
+    }
+
+    const handleDragEnter = (e) => {
+        console.log("entering drag..")
+    }
+
+    const getStyles = (params) => {
+        const currentItem = dragItem.current;
+        if (currentItem.grpI === params.grpI && currentItem.itemI === params.itemI) {
+            return 'current dnd-item';
+        }
+        return 'dnd-item';
     }
 
     return (
@@ -13,7 +42,12 @@ export const DragNDrop = ({ data }) => {
                 <div key={grp.title} className='dnd-group'>
                     <div className='group-title'> {grp.title}</div>
                     {grp.items.map((item, itemI) => (
-                        <div draggable onDragStart={(e) => { handleDragStart(e, { grpI, itemI }) }} key={item} className='dnd-item'>
+                        <div
+                            draggable
+                            onDragStart={(e) => { handleDragStart(e, { grpI, itemI }) }}
+                            onDragEnter={dragging ? handleDragEnter : null}
+                            key={item}
+                            className={dragging ? getStyles({ grpI, itemI }) : 'dnd-item'}>
                             {item}
                         </div>
                     ))}
